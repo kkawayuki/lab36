6379c0252c5132bf1a5b9c15cf19c7685c0e962d
kkawayuki <kyukiokawa@gmail.com>
2024-12-01 20:04:53 -0800

    2

--- main.cpp
+++ main.cpp
@@ -1,22 +1,39 @@
 // COMSC-210 | lab 36 | Kent Kawashima
 // IDE used: Visual Studio Code
 #include <iostream>
-#include "IntBinaryTree.h"
+#include "IntBinaryTree.h" //this is actually a string binary tree now
 #include <fstream> //for file input/output
 using namespace std;
 
+//function prototype
+void readIn(IntBinaryTree &root);
+
+/************************************************
+ * Function: Main
+ ************************************************/
 int main()
 {
     //add string functionality with values
-    
+
     // add menu-driven functionality
     // add/delete/search/modify records
 
-    cout << "test";
+    IntBinaryTree root = IntBinaryTree(); //initalize root to locaiton of binary tree
+
+    //readIn(root); 
+    
 
     return 0;
 }
 
-void readIn()
+
+void readIn(IntBinaryTree &root)
 {
+    string file = "text.txt", buf;
+    ifstream fin(file); 
+
+    while(getline(fin,buf)) //for each line
+    {
+        root.insertNode(buf); //insert into binary tree
+    }
 }
