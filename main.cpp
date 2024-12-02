dac002c0db919e84e2faf8a004ba1575bdd5154f
kkawayuki <kyukiokawa@gmail.com>
2024-12-01 20:38:49 -0800

    5

--- main.cpp
+++ main.cpp
@@ -2,81 +2,106 @@
 // IDE used: Visual Studio Code
 #include <iostream>
 #include "IntBinaryTree.h" //this is actually a string binary tree now
-#include <fstream> //for file input/output
+#include <fstream>         //for file input/output
 using namespace std;
 
-//function prototype
+// function prototype
 void readIn(IntBinaryTree &);
 int printMenu();
+void addValue(IntBinaryTree &);
+void deleteValue(IntBinaryTree &);
+void searchValue(IntBinaryTree &);
+void modifyValue(IntBinaryTree &);
+
 
 /************************************************
  * Function: Main
  ************************************************/
 int main()
 {
-    //add string functionality with values
+    // build binary tree
+    IntBinaryTree myTree = IntBinaryTree();
 
-    // add menu-driven functionality
-    // add/delete/search/modify records
+    // logic to read in values to BST
+    readIn(myTree);
 
-    IntBinaryTree myTree = IntBinaryTree(); //initalize root to locaiton of binary tree
+    int choice = 0;
 
-    //logic to read in values to BST
-    readIn(myTree); 
-    
-    int choice;
-
-    while(choice != 5);
+    while (choice != 5)
     {
         choice = printMenu();
-        switch(choice)
+        switch (choice)
         {
-            case(1):
-                break;
+        case (1):
+            addValue(myTree);
+            break;
 
-            case(2):
-                break;
+        case (2):
+            deleteValue(myTree);
+            break;
 
-            case(3):
-                break;
-            
-            case(4):
-                break;
+        case (3):
+            searchValue(myTree);
+            break;
 
-            default: //shouldn't be possible due to error handling in printMenu()
-                break;
+        case (4):
+            modifyValue(myTree); 
+            break;
+
+        default: // shouldn't be possible due to range handling in printMenu()
+            break;
         }
     }
-    
-    
-    //myTree.displayInOrder(); //doesn't require parameter, uses private root variable
 
-    cout << myTree.searchNode("a");
+    // myTree.displayInOrder(); //doesn't require parameter, uses private root variable
 
     return 0;
 }
 
-
 void readIn(IntBinaryTree &myTree)
 {
     string file = "codes.txt", buf;
-    ifstream fin(file); 
+    ifstream fin(file);
 
-    while(getline(fin,buf)) //for each line
+    while (getline(fin, buf)) // for each line
     {
-        myTree.insertNode(buf); //insert into binary tree
+        myTree.insertNode(buf); // insert into binary tree
     }
 }
 
 int printMenu()
 {
     int choice = 0;
+    cout << "BST MENU -----\n1) Add a Node\n2) Delete a Node\n3) Search for a Node\n4) Modify a Node\n5) Quit Program\nYour Choice: -----> ";
+    cin >> choice;
 
-    while(choice < 0 || choice > 5)
+    // validation loop
+    while (choice < 0 || choice > 5)
     {
-        cout << "BST MENU -----\n1) Add a Node\n2)Delete a Node\n3)Search for a Node\n4)Modify a Node\nYour Choice: ----->"; 
-        cin >> choice; 
+        cout << "ERROR: Value must be from 1-5!\n\n";
+        cout << "BST MENU -----\n1) Add a Node\n2) Delete a Node\n3) Search for a Node\n4) Modify a Node\n5) Quit Program\nYour Choice: -----> ";
+        cin >> choice;
     }
     cout << '\n';
-    return(choice);
+    return (choice);
+}
+
+void addValue(IntBinaryTree &myTree)
+{
+
+}
+
+void deleteValue(IntBinaryTree &myTree)
+{
+
+}
+
+void searchValue(IntBinaryTree &myTree)
+{
+
+}
+
+void modifyValue(IntBinaryTree &myTree)
+{
+
 }
